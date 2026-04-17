cb6e608876ccad93f1cbac12c6f6a04ccbf2f668
sumit mahant <sumitmahant31@gmail.com>
2026-04-17 19:24:20 +0530

    ffssfdfsf

--- second.c
+++ second.c
@@ -1,22 +1,25 @@
 # include<stdio.h>
 int main(){
 //radius of a circle 3.14*r*r 
-float radius;
-printf("radius of the circle-:");
-scanf("%f",& radius );
-printf("\narea is %f",radius*3.14*radius);
-//perimeter of ractangle 2(w+h)
- float side1;
- float side2;
- printf("enter the hight-: ");
- scanf("%f",&side1);
-printf("enter the width-: ");
- scanf("%f",&side2);
- printf("perimeter of ractangle is-: %f", 2*(side1+side2));
- //cube of a number a*a*a 
- int num;
- printf("Enter the number ");
- scanf("%d",&num);
- printf("cube of the number is -:%d", num*num*num);
+// float radius;
+// printf("radius of the circle-:");
+// scanf("%f",& radius );
+// printf("\narea is %f",radius*3.14*radius);
+// //perimeter of ractangle 2(w+h)
+//  float side1;
+//  float side2;
+//  printf("enter the hight-: ");
+//  scanf("%f",&side1);
+// printf("enter the width-: ");
+//  scanf("%f",&side2);
+//  printf("perimeter of ractangle is-: %f", 2*(side1+side2));
+//  //cube of a number a*a*a 
+//  int num;
+//  printf("Enter the number ");
+//  scanf("%d",&num);
+//  printf("cube of the number is -:%d", num*num*num);
+ int a,b,c;
+ a=b=c=5;
+ printf("%d",a);
 
 }
