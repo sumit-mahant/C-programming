e6b3e9383235e5085bb58e9ba3dc7af9133a07c0
sumit mahant <sumitmahant31@gmail.com>
2026-04-18 20:52:00 +0530

    kjbkjhk

--- array.c
+++ array.c
@@ -1,5 +1,21 @@
 # include<stdio.h>
 int main(){
-printf("sumit");
+int arr[3][3];
+for(int i=0;i<3;i++)
+{
+    for (int j=0;j<3;j++)
+{
+    scanf("%d",&arr[i][j]);
+}
+
+}
+for(int i=0;i<3;i++)
+{
+    for (int j=0;j<3;j++)
+{
+    printf("%d\n",arr[i][j]);
+}
+
+}
 
 }
