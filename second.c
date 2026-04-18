9b269d1c323acd48296cba8472e60b6bb7f740fd
sumit mahant <sumitmahant31@gmail.com>
2026-04-18 09:42:30 +0530

    adsfsf

--- second.c
+++ second.c
@@ -18,8 +18,24 @@ int main(){
 //  printf("Enter the number ");
 //  scanf("%d",&num);
 //  printf("cube of the number is -:%d", num*num*num);
- int a,b,c;
- a=b=c=5;
- printf("%d",a);
+//  int a,b,c;
+//  a=b=c=5;
+//  printf("%d",a);
+int arr[3][3];
+for(int i=0;i<3;i++)
+{
+    for (int j=0;j<3;j++)
+{
+    scanf("%d",&arr[i][j]);
+}
 
+}
+for(int i=0;i<3;i++)
+{
+    for (int j=0;j<3;j++)
+{
+    printf("%d\n",arr[i][j]);
+}
+
+}
 }
