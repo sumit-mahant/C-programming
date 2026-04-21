c324e8294789197d807e9833220c175fbc12c1fc
sumit mahant <sumitmahant31@gmail.com>
2026-04-21 18:25:34 +0530

    fsdfsdfs

--- second.c
+++ second.c
@@ -48,14 +48,15 @@ int main(){
 // scanf("%d %d %d",&num1,&num2,&num3);
 // average=(num1+num2+num3)/3;
 // printf("average=%f",average);
-int age;
-printf("Enter your age-:");
-scanf("%d",&age);
-if(age>18){
-    printf("\nyou are an adult");
-}
-else{
-    printf("you are a teenager");
-}
+// int age;
+// printf("Enter your age-:");
+// scanf("%d",&age);
+// if(age>18){
+//     printf("\nyou are an adult");
+// }
+// else{
+//     printf("you are a teenager");
+
+// }
 
 }
