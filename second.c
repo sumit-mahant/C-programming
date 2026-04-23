93572bcea7c1209f9bbd0e4e98d4007ac692964b
sumit mahant <sumitmahant31@gmail.com>
2026-04-23 10:43:08 +0530

    sfasf

--- second.c
+++ second.c
@@ -113,22 +113,22 @@ int main()
     // else{
     // printf("negative");
     // }
-    int arr[3][3];
-    int sum = 0;
-    for (int i = 0; i < 3; i++)
-    {
-        for (int j = 0; j < 3; j++)
-        {
-            scanf("%d", &arr[i][j]);
-        }
-    }
-    for (int i = 0; i < 3; i++)
-    {
-        for (int j = 0; j < 3; j++)
-        {
-            sum = sum + arr[i][j];
-        }
-    }
+    // int arr[3][3];
+    // int sum = 0;
+    // for (int i = 0; i < 3; i++)
+    // {
+    //     for (int j = 0; j < 3; j++)
+    //     {
+    //         scanf("%d", &arr[i][j]);
+    //     }
+    // }
+    // for (int i = 0; i < 3; i++)
+    // {
+    //     for (int j = 0; j < 3; j++)
+    //     {
+    //         sum = sum + arr[i][j];
+    //     }
+    // }
 
-    printf("%d", sum);
+    // printf("%d", sum);
 }
