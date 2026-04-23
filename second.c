573f760ba2f95c02c0365c5eff4b2ebd1de214e0
sumit mahant <sumitmahant31@gmail.com>
2026-04-23 10:40:04 +0530

    uhhjkhk

--- second.c
+++ second.c
@@ -1,134 +1,134 @@
-# include<stdio.h>
+#include <stdio.h>
 // # include<math.h>
-int main(){
-//radius of a circle 3.14*r*r 
-// float radius;
-// printf("radius of the circle-:");
-// scanf("%f",& radius );
-// printf("\narea is %f",radius*3.14*radius);
-// //perimeter of ractangle 2(w+h)
-//  float side1;
-//  float side2;
-//  printf("enter the hight-: ");
-//  scanf("%f",&side1);
-// printf("enter the width-: ");
-//  scanf("%f",&side2);
-//  printf("perimeter of ractangle is-: %f", 2*(side1+side2));
-//  //cube of a number a*a*a 
-//  int num;
-//  printf("Enter the number ");
-//  scanf("%d",&num);
-//  printf("cube of the number is -:%d", num*num*num);
-//  int a,b,c;
-//  a=b=c=5;
-//  printf("%d",a);
-// int a=3,b=2;
-// int sum=(a+b);
-// printf("%d\n",sum);
-// int power =pow(a,b);
-// printf("%d\n",power);
-// printf("%d\n",a%b);
-// printf("%d\n",16%3);
-// float d=(int) 5.99999999;
-// int f=2.34567;
-// printf("%d\n",f);
-// printf("%f",d);
-// int p=12+2*3;
-// printf("%d",p);
-// printf("\n%d",20/5-3);
-// printf("\n%d",20==1);
-// printf("\n%d",20!=20);
-// printf("\n%d",20!=20);
-// printf("\n%d",!(20!=20||2<3));
-//  printf("\n%d",!(20!=20));
-
-// int num1,num2,num3;
-// float average;
-// printf("Enter the three number-:");
-// scanf("%d %d %d",&num1,&num2,&num3);
-// average=(num1+num2+num3)/3;
-// printf("average=%f",average);
-// int age;
-// printf("Enter your age-:");
-// scanf("%d",&age);
-// if(age>18){
-//     printf("\nyou are an adult");
-// }
-// else{
-//     printf("you are a teenager");
-
-// }
-
-// int marks;
-// printf("Enter your marks");
-// scanf("%d",&marks);
-// if(marks>=90){
-//    printf("excellent  student\n");
-// }
-// else if(marks>=80 ){
-//     printf("you are very good student\n ");
-
-// }
-// else if(marks>=70 ){
-//     printf("you are a good student, you could do better\n  ");
-// }
-// else if(marks>=50 ){
-//     printf("you are a average student you should have do better\n  ");
-// }
-// else{
-//     printf("you are fail you have to do better\n");
-// }
-// int day;
-// printf("Enter the day(1-7)-:");
-// scanf("%d",&day);
-// switch (day){
-//     case 1: printf("Monday\n");
-//            break;
-//     case 2:printf("Tuesday\n");
-//            break;
-//     case 3:printf("Wednsday\n");
-//            break;
-//     case 4: printf("Thursday\n");
-//             break;
-//     case 5:printf("Friday\n");
-//            break;
-//     case 6:printf("Saturday\n");
-//           break;
-//     case 7:printf("sunday");
-//           break;
-//     default : printf("this is not a valid day ");
-// int num;
-// printf("Enter the number-:");
-// scanf("%d",&num);
-// if(num>0){
-//     printf(" positive\n");
-//     if(num%2==0){
-//       printf("even");
-//     }
-//     else{
-//         printf("odd");
-//     }
-//     }
-// else{
-    // printf("negative");
-// }
-int arr[3][3];
-int sum;
-for (int i = 0; i < 3; i++)
+int main()
 {
-   for (int j = 0; j < 3; j++)
-   {
-    scanf("%d",&arr[i][j]);
-   }
-   for(int i=0;i<3;i++)
-   {
-    for(int j=0;j<3;j++)
-    sum=sum+arr[i][j];
-   }
+    // radius of a circle 3.14*r*r
+    //  float radius;
+    //  printf("radius of the circle-:");
+    //  scanf("%f",& radius );
+    //  printf("\narea is %f",radius*3.14*radius);
+    //  //perimeter of ractangle 2(w+h)
+    //   float side1;
+    //   float side2;
+    //   printf("enter the hight-: ");
+    //   scanf("%f",&side1);
+    //  printf("enter the width-: ");
+    //   scanf("%f",&side2);
+    //   printf("perimeter of ractangle is-: %f", 2*(side1+side2));
+    //   //cube of a number a*a*a
+    //   int num;
+    //   printf("Enter the number ");
+    //   scanf("%d",&num);
+    //   printf("cube of the number is -:%d", num*num*num);
+    //   int a,b,c;
+    //   a=b=c=5;
+    //   printf("%d",a);
+    //  int a=3,b=2;
+    //  int sum=(a+b);
+    //  printf("%d\n",sum);
+    //  int power =pow(a,b);
+    //  printf("%d\n",power);
+    //  printf("%d\n",a%b);
+    //  printf("%d\n",16%3);
+    //  float d=(int) 5.99999999;
+    //  int f=2.34567;
+    //  printf("%d\n",f);
+    //  printf("%f",d);
+    //  int p=12+2*3;
+    //  printf("%d",p);
+    //  printf("\n%d",20/5-3);
+    //  printf("\n%d",20==1);
+    //  printf("\n%d",20!=20);
+    //  printf("\n%d",20!=20);
+    //  printf("\n%d",!(20!=20||2<3));
+    //   printf("\n%d",!(20!=20));
+
+    // int num1,num2,num3;
+    // float average;
+    // printf("Enter the three number-:");
+    // scanf("%d %d %d",&num1,&num2,&num3);
+    // average=(num1+num2+num3)/3;
+    // printf("average=%f",average);
+    // int age;
+    // printf("Enter your age-:");
+    // scanf("%d",&age);
+    // if(age>18){
+    //     printf("\nyou are an adult");
+    // }
+    // else{
+    //     printf("you are a teenager");
+
+    // }
+
+    // int marks;
+    // printf("Enter your marks");
+    // scanf("%d",&marks);
+    // if(marks>=90){
+    //    printf("excellent  student\n");
+    // }
+    // else if(marks>=80 ){
+    //     printf("you are very good student\n ");
+
+    // }
+    // else if(marks>=70 ){
+    //     printf("you are a good student, you could do better\n  ");
+    // }
+    // else if(marks>=50 ){
+    //     printf("you are a average student you should have do better\n  ");
+    // }
+    // else{
+    //     printf("you are fail you have to do better\n");
+    // }
+    // int day;
+    // printf("Enter the day(1-7)-:");
+    // scanf("%d",&day);
+    // switch (day){
+    //     case 1: printf("Monday\n");
+    //            break;
+    //     case 2:printf("Tuesday\n");
+    //            break;
+    //     case 3:printf("Wednsday\n");
+    //            break;
+    //     case 4: printf("Thursday\n");
+    //             break;
+    //     case 5:printf("Friday\n");
+    //            break;
+    //     case 6:printf("Saturday\n");
+    //           break;
+    //     case 7:printf("sunday");
+    //           break;
+    //     default : printf("this is not a valid day ");
+    // int num;
+    // printf("Enter the number-:");
+    // scanf("%d",&num);
+    // if(num>0){
+    //     printf(" positive\n");
+    //     if(num%2==0){
+    //       printf("even");
+    //     }
+    //     else{
+    //         printf("odd");
+    //     }
+    //     }
+    // else{
+    // printf("negative");
+    // }
+    int arr[3][3];
+    int sum = 0;
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            scanf("%d", &arr[i][j]);
+        }
+    }
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            sum = sum + arr[i][j];
+        }
+    }
+
+    printf("%d", sum);
 }
-printf("sum is =%d",sum);
-
-}
-
-
-
