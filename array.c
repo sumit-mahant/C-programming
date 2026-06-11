ccda02f455bdae359993e93eb95c2fbd4c413e6c
sumit mahant <sumitmahant31@gmail.com>
2026-06-11 21:36:03 +0530

    afa

--- array.c
+++ array.c
@@ -2,5 +2,18 @@
 int main(){
 int a=10;
 int b=a+10;
-printf("%d",b);
+printf("%d\n",b);
+int c=2,d=3;
+printf("%d\n",c+d);
+int z=1.888888;
+printf("%d\n",z);
+printf("%d",!(5>2));
+int x;
+printf("Enter the number -:\n");
+//scanf("%d",&x);
+printf("%d",(x>9&&x<100));
+int r=4;int p=2;int y=6;
+printf("%d",(r+p+y)/3);
+
+
 }
