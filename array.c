5b3355d1c807630dc09f649731265c0e459dc94e
sumit mahant <sumitmahant31@gmail.com>
2026-06-13 18:24:20 +0530

    ldkg;ldk

--- array.c
+++ array.c
@@ -1,19 +1,36 @@
 # include<stdio.h>
 int main(){
-int a=10;
-int b=a+10;
-printf("%d\n",b);
-int c=2,d=3;
-printf("%d\n",c+d);
-int z=1.888888;
-printf("%d\n",z);
-printf("%d",!(5>2));
-int x;
-printf("Enter the number -:\n");
-//scanf("%d",&x);
-printf("%d",(x>9&&x<100));
-int r=4;int p=2;int y=6;
-printf("%d",(r+p+y)/3);
+// int a=10;
+// int b=a+10;
+// printf("%d\n",b);
+// int c=2,d=3;
+// printf("%d\n",c+d);
+// int z=1.888888;
+// printf("%d\n",z);
+// printf("%d",!(5>2));
+// int x;
+// printf("Enter the number -:\n");
+// //scanf("%d",&x);
+// printf("%d",(x>9&&x<100));
+// int r=4;int p=2;int y=6;
+// printf("%d",(r+p+y)/3);
+// int age;
+// printf("Enter your age\n");
+// scanf("%d",&age);
+// if(age<18){
+//     printf("YOU can not drive ");
+// }
+// else if (age>18&&age<30){
+//     printf("Yes you are young .");
+// }
+// else{
+//     printf("YOU can  drive ");
+// }
+//ternary operator
+//condition?do something if true :do something if false 
+// age>18?printf("adult\n"):printf("miner");
+
+
 
 
 }
