5722436a1518320990d997af2dd8cef5325ce045
sumit mahant <sumitmahant31@gmail.com>
2026-06-18 23:19:02 +0530

    sffsdf

--- array.c
+++ array.c
@@ -207,19 +207,36 @@ int main(){
 //      }
      
 //   printf("\n");}
-int num;
-printf("Enter a number:-");
-scanf("%d",&num);
-if(num<=1){
-     printf("NOt PRIME");
+// int num;
+// printf("Enter a number:-");
+// scanf("%d",&num);
+// if(num<=1){
+//      printf("NOt PRIME");
+// }
+// for(int i=2;i<num;i++){
+//      if(num%i!=0){
+//           printf("  prime number");
+//      }else{
+//           printf("NOt prime ");
+//      }
+// }
+//CHECK FOR A PRIME NUMBER 
+int n;
+printf("Enter the number-:");
+scanf("%d",&n);
+if(n<=1){
+     printf("NOT PRIME");
 }
-for(int i=2;i<num;i++){
-     if(num%i!=0){
-          printf("  prime number");
-     }else{
-          printf("NOt prime ");
+for(int i=2;i*i<=n;i++){
+     if(n%i==0){
+          printf("NOT PRIME");
      }
-}
+printf("prime number");}
+
+// } printf("prime number");
+
+
+
 }
 
 
