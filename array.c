91312546bbdc5a4086ccdb22bfa3cde827fad655
sumit mahant <sumitmahant31@gmail.com>
2026-07-12 17:43:54 +0530

    sdfsfsf

--- array.c
+++ array.c
@@ -1,258 +1,258 @@
 # include<stdio.h>
-// int main(){
-// int a=10;
-// int b=a+10;
-// printf("%d\n",b);
-// int c=2,d=3;
-// printf("%d\n",c+d);
-// int z=1.888888;
-// printf("%d\n",z);
-// printf("%d",!(5>2));
-// int x;
-// printf("Enter the number -:\n");
-// //scanf("%d",&x);
-// printf("%d",(x>9&&x<100));
-// int r=4;int p=2;int y=6;
-// printf("%d",(r+p+y)/3);
-// int age;
-// printf("Enter your age\n");
-// scanf("%d",&age);
-// if(age<18){
-//     printf("YOU can not drive ");
-// }
-// else if (age>18&&age<30){
-//     printf("Yes you are young .");
-// }
-// else{
-//     printf("YOU can  drive ");
-// }
-//ternary operator
-//condition?do something if true :do something if false 
-// age>18?printf("adult\n"):printf("miner");
-// int days ;//1-mon,2-tue,3-wed.....
-// printf("Enter a day(1-7)"),
-// scanf("%d",&days);
-// switch(days){
-//     case 1 :printf("Monday\n");
-//     break;
-//      case 2 :printf("tueday\n");
-//     break;
-//      case 3 :printf("wednsday\n");
-//     break;
-//      case 4 :printf("thursday\n");
-//     break;
-//      case 5 :printf("friday\n");
-//     break;
-//      case 6 :printf("saturday\n");
-//     break;
-//      case 7 :printf("sunday\n");
-//     break;
-//     default:printf("not a valid day  ");
+int main(){
+int a=10;
+int b=a+10;
+printf("%d\n",b);
+int c=2,d=3;
+printf("%d\n",c+d);
+int z=1.888888;
+printf("%d\n",z);
+printf("%d",!(5>2));
+int x;
+printf("Enter the number -:\n");
+//scanf("%d",&x);
+printf("%d",(x>9&&x<100));
+int r=4;int p=2;int y=6;
+printf("%d",(r+p+y)/3);
+int age;
+printf("Enter your age\n");
+scanf("%d",&age);
+if(age<18){
+    printf("YOU can not drive ");
+}
+else if (age>18&&age<30){
+    printf("Yes you are young .");
+}
+else{
+    printf("YOU can  drive ");
+}
+ternary operator
+condition?do something if true :do something if false 
+age>18?printf("adult\n"):printf("miner");
+int days ;//1-mon,2-tue,3-wed.....
+printf("Enter a day(1-7)"),
+scanf("%d",&days);
+switch(days){
+    case 1 :printf("Monday\n");
+    break;
+     case 2 :printf("tueday\n");
+    break;
+     case 3 :printf("wednsday\n");
+    break;
+     case 4 :printf("thursday\n");
+    break;
+     case 5 :printf("friday\n");
+    break;
+     case 6 :printf("saturday\n");
+    break;
+     case 7 :printf("sunday\n");
+    break;
+    default:printf("not a valid day  ");
 
-// int num;
-// printf("Enter a number "),
-// scanf("%d",&num);
-// if(num>=0){
-//     printf("number is positive\n");
-//     if(num%2==0){
-//         printf("the number is also even");
-//     }else{
-//         printf("the number is odd");
-//     }
-// }else{
-//     printf("the number is negative");
-// }
+int num;
+printf("Enter a number "),
+scanf("%d",&num);
+if(num>=0){
+    printf("number is positive\n");
+    if(num%2==0){
+        printf("the number is also even");
+    }else{
+        printf("the number is odd");
+    }
+}else{
+    printf("the number is negative");
+}
 
 
 
-// }
-// int marks;
-// printf("Enter the marks");
-// scanf("%d",&marks);
-// if(marks>30){
-//     printf("the student is pass \n");
-// }else{
-//     printf("the student is fail");
-// }
-// marks <=30?printf("fail"):printf("Pass");
-// if(marks<30){
-//     printf("your grade is 'c'");
-// }else if (marks>=30&&marks<70){
-//     printf("your grade is 'B'");
-// }else if (marks>=70&&marks<90){
-//     printf("your grade is 'A'");
-// }else if(marks>=90&&marks<=100){
-//     printf("your grade is 'A+");
-// }
-// else{
-//     printf("the number is invalid ");
-// }
+}
+int marks;
+printf("Enter the marks");
+scanf("%d",&marks);
+if(marks>30){
+    printf("the student is pass \n");
+}else{
+    printf("the student is fail");
+}
+marks <=30?printf("fail"):printf("Pass");
+if(marks<30){
+    printf("your grade is 'c'");
+}else if (marks>=30&&marks<70){
+    printf("your grade is 'B'");
+}else if (marks>=70&&marks<90){
+    printf("your grade is 'A'");
+}else if(marks>=90&&marks<=100){
+    printf("your grade is 'A+");
+}
+else{
+    printf("the number is invalid ");
+}
 
-// for check weather the character is uppercase OR lowercase.
-// char ch;
-// printf("Enter the charcter ");
-// scanf("%c",&ch);
+for check weather the character is uppercase OR lowercase.
+char ch;
+printf("Enter the charcter ");
+scanf("%c",&ch);
 
-// if(ch>='a'&&ch<='z'){
-//     printf("the charcter in 'Lowercase' ");
-// }else if(ch>='A'&&ch<='Z'){
-//     printf("the character in 'Uppercase' ");
-// }else{
-//     printf("not English "); 
-// }
+if(ch>='a'&&ch<='z'){
+    printf("the charcter in 'Lowercase' ");
+}else if(ch>='A'&&ch<='Z'){
+    printf("the character in 'Uppercase' ");
+}else{
+    printf("not English "); 
+}
 
-// for(int i=0; i<=10; i= i+2) {
-//     printf("%d \n",i);
-// }
-// for(int i=10; i>=1; i=i-1){
-//     printf("%d\n",i);
-// }
-// int n;
-// printf("Enter a number-:");
-// scanf("%d",&n);
-// for(int i=1;i<=20;i++){
-//     printf("%d\n",n*i);
-// }
-// for(int i=0;i<=6;i++){
-//     if(i==5){
-//         break;
-//     }
-//     printf("%d\n",i);
-// }
-// for(int i=0;i<=6;i++){
-//     if(i==10){
-//         break;
-//     }
-//     printf("%d\n",i);
-// }
-// int n;
-// do
-// {
-//     printf("Enter a number-:");
-//     scanf("%d",&n);
-//     if(n%2!=0){
-//         break;
-//     }
-// } while (1);
-// int n ;
-// do
-// {
-//     printf("Enter a number-:");
-//     scanf("%d",&n);
-//     if(n%7==0){
-//         break;
-//     }
+for(int i=0; i<=10; i= i+2) {
+    printf("%d \n",i);
+}
+for(int i=10; i>=1; i=i-1){
+    printf("%d\n",i);
+}
+int n;
+printf("Enter a number-:");
+scanf("%d",&n);
+for(int i=1;i<=20;i++){
+    printf("%d\n",n*i);
+}
+for(int i=0;i<=6;i++){
+    if(i==5){
+        break;
+    }
+    printf("%d\n",i);
+}
+for(int i=0;i<=6;i++){
+    if(i==10){
+        break;
+    }
+    printf("%d\n",i);
+}
+int n;
+do
+{
+    printf("Enter a number-:");
+    scanf("%d",&n);
+    if(n%2!=0){
+        break;
+    }
+} while (1);
+int n ;
+do
+{
+    printf("Enter a number-:");
+    scanf("%d",&n);
+    if(n%7==0){
+        break;
+    }
     
-// } while (1);
-// printf("thank you");
-// for ( int i = 0; i <=10; i++)
-// {
-//     if (i==5)
-//     {
-//         continue;//for skip the itration !
-//     }
+} while (1);
+printf("thank you");
+for ( int i = 0; i <=10; i++)
+{
+    if (i==5)
+    {
+        continue;//for skip the itration !
+    }
     
-//     printf("%d\n",i);
-// }
-// for(int i=5;i<=50;i++){
-//     if(i%2==0){
-//         continue;
-//     }
-//     printf("%d\n",i);
-// }
+    printf("%d\n",i);
+}
+for(int i=5;i<=50;i++){
+    if(i%2==0){
+        continue;
+    }
+    printf("%d\n",i);
+}
 
-// for(int i=5;i<=50;i=i+2){
+for(int i=5;i<=50;i=i+2){
     
-//     printf("%d\n",i);
-// }
-//factorial of a number !
-// int n;
-// printf("Enter a number-:");
-// scanf("%d",&n);
-// int fact=1;
-// for(int i=1;i<=n;i++){
-//     fact=fact*i;
+    printf("%d\n",i);
+}
+factorial of a number !
+int n;
+printf("Enter a number-:");
+scanf("%d",&n);
+int fact=1;
+for(int i=1;i<=n;i++){
+    fact=fact*i;
     
-// }printf("%d\n",fact);
-// int n;
-// printf("Enter a number-:");
-// scanf("%d",&n);
-// for(int i=10;i>=1;i--){
-//     printf("%d\n",n*i);
-// }
-// int sum=0;
-// for(int i=5;i<=50;i++){
-//      sum=sum+i;
-// }
-// printf("%d",sum);
-// int i,j;
-// for(int i=1;i<=8;i++){
-//      for(int j=1;j<=5;j++){
-//           printf("* ");
-//      }
-// printf("\n");}
-// printf("new one\n");
-// int k,s;
-// for(int k=1;k<=2;k++){
-//      for(int s=6;s>=1;s--){
-//           printf("* ");
-//      }printf("\n");
-// }
-// int p,r;
-// for(int p=1;p<=5;p++)
-// {
-//      for(int r=1;r<=p;r++)
-//      {
-//           printf("* ");
-//      }
+}printf("%d\n",fact);
+int n;
+printf("Enter a number-:");
+scanf("%d",&n);
+for(int i=10;i>=1;i--){
+    printf("%d\n",n*i);
+}
+int sum=0;
+for(int i=5;i<=50;i++){
+     sum=sum+i;
+}
+printf("%d",sum);
+int i,j;
+for(int i=1;i<=8;i++){
+     for(int j=1;j<=5;j++){
+          printf("* ");
+     }
+printf("\n");}
+printf("new one\n");
+int k,s;
+for(int k=1;k<=2;k++){
+     for(int s=6;s>=1;s--){
+          printf("* ");
+     }printf("\n");
+}
+int p,r;
+for(int p=1;p<=5;p++)
+{
+     for(int r=1;r<=p;r++)
+     {
+          printf("* ");
+     }
      
-//   printf("\n");}
-// int num;
-// printf("Enter a number:-");
-// scanf("%d",&num);
-// if(num<=1){
-//      printf("NOt PRIME");
-// }
-// for(int i=2;i<num;i++){
-//      if(num%i!=0){
-//           printf("  prime number");
-//      }else{
-//           printf("NOt prime ");
-//      }
-// }
-//
-// int n;
-// printf("Enter the number-:");
-// scanf("%d",&n);
-// if(n<=1){
-//      printf("NOT PRIME");
-// }
-// for(int i=2;i*i<=n;i++){
-//      if(n%i==0){
-//           printf("NOT PRIME");
-//      }
-// printf("prime number");}
+  printf("\n");}
+int num;
+printf("Enter a number:-");
+scanf("%d",&num);
+if(num<=1){
+     printf("NOt PRIME");
+}
+for(int i=2;i<num;i++){
+     if(num%i!=0){
+          printf("  prime number");
+     }else{
+          printf("NOt prime ");
+     }
+}
 
-// } printf("prime number");
-//CHECK FOR A PRIME NUMBER 
-// int num,i,prime=1;
-// printf("Enter a number:-");
-// scanf("%d",&num);
-// if(num<=1){
-//     prime=0;
-// }else{
-// for(i=2;i*i<=num;i++){
-//      if(num%i==0){
-//           prime=0;
-//           break;
-//      }
-// }
-// }
-// if(prime){
-//      printf("%d is a prime number\n ",num);
-// }else{
-//      printf("%d is not a prime number",num );
-// }
+int n;
+printf("Enter the number-:");
+scanf("%d",&n);
+if(n<=1){
+     printf("NOT PRIME");
+}
+for(int i=2;i*i<=n;i++){
+     if(n%i==0){
+          printf("NOT PRIME");
+     }
+printf("prime number");}
+
+} printf("prime number");
+CHECK FOR A PRIME NUMBER 
+int num,i,prime=1;
+printf("Enter a number:-");
+scanf("%d",&num);
+if(num<=1){
+    prime=0;
+}else{
+for(i=2;i*i<=num;i++){
+     if(num%i==0){
+          prime=0;
+          break;
+     }
+}
+}
+if(prime){
+     printf("%d is a prime number\n ",num);
+}else{
+     printf("%d is not a prime number",num );
+}
 // FUNCTION!!!!!
 int a=10;
 void num();
