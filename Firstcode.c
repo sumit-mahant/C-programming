3b877ab5b39b611473ffc7e3d4c37533fc77b7ff
sumit mahant <sumitmahant31@gmail.com>
2026-07-12 17:54:15 +0530

    skfj

--- Firstcode.c
+++ Firstcode.c
@@ -1,33 +1,40 @@
 #include <stdio.h> 
 int main(){
+#include<math.h>
+// printf("sumit\n");
 
-printf("sumit\n");
-
-int number=25;
- printf("%d",number);
-// this is the single line comment.
-/*this
-is the 
-multiple line comment */
-char star='*';
-printf("\n%c",star);
-float pi=3.14;
-printf("\n%f",pi);
-int num ;
-printf("\nenter your number :");
-scanf("%d",&num);
-printf("\nthe number is %d\n",num);
-int a,b;
-printf("enter a number ");
-scanf("%d",&a);
-printf("enter  the second number ");
-scanf("%d",&b);
-int sum=a+b;
-printf("the sum is %d",sum);
+// int number=25;
+//  printf("%d",number);
+// // this is the single line comment.
+// /*this
+// is the 
+// multiple line comment */
+// char star='*';
+// printf("\n%c",star);
+// float pi=3.14;
+// printf("\n%f",pi);
+// int num ;
+// printf("\nenter your number :");
+// scanf("%d",&num);
+// printf("\nthe number is %d\n",num);
+// int a,b;
+// printf("enter a number ");
+// scanf("%d",&a);
+// printf("enter  the second number ");
+// scanf("%d",&b);
+// int sum=a+b;
+// printf("the sum is %d",sum);
 
 
-// int age =22;
+// // int age =22;
 // printf("\nthis is my name ");
+int a=2;
+int b=10;
+int power=pow(a,b);
+printf("%d",power);
+
+
+
 
 return 0;
 }
