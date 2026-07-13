5654fb834aa1141d7eb933850704c41d8c4cc153
sumit mahant <sumitmahant31@gmail.com>
2026-07-13 23:31:13 +0530

    26

--- second.c
+++ second.c
@@ -2,4 +2,15 @@
 void printhello();
 int main()
 {
+    printhello();
+printhello();
+printhello();
+printhello();
+printhello();
+printhello();
+
+
+}
+void printhello(){
+    printf("hello world!\n");
 }
