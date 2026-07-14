a9fb2a78921a1f5fd902bf08abf5d64a33c0ff6d
sumit mahant <sumitmahant31@gmail.com>
2026-07-14 11:01:28 +0530

    01

--- second.c
+++ second.c
@@ -1,22 +1,36 @@
 #include <stdio.h>
-void printhello();
-void printgoodbye();
-int main()
-{
-    printhello();
-printhello();
-printhello();
-printhello();
-printhello();
-printhello();
-printgoodbye();
-printgoodbye();
-printgoodbye();
-printgoodbye();
-printgoodbye();
+// void printhello();
+// void printgoodbye();
+// int main()
+// {
+//     printhello();
+// printhello();
+// printhello();
+// printhello();
+// printhello();
+// printhello();
+// printgoodbye();
+// printgoodbye();
+// printgoodbye();
+// printgoodbye();
+// printgoodbye();
+// }
+// void printhello(){
+//     printf("I am sumit bro\nI am the best \nI win\nI do't lose\n");
+// }void printgoodbye(){
+//     printf("goodbye baby\n");
+// }
+
+void namaste();
+void monjour();
+
+
+int main (){
+
 }
-void printhello(){
-    printf("I am sumit bro\nI am the best \nI win\nI do't lose\n");
-}void printgoodbye(){
-    printf("goodbye baby\n");
+void namaste(){
+    printf("namste indian boy!\n");
+}
+void monjour(){
+    printf("monjour franch man!\n");
 }
