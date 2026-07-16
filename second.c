2673071cbaae17b821629fd282b28419866024d6
sumit mahant <sumitmahant31@gmail.com>
2026-07-16 16:52:12 +0530

    kkkkk

--- second.c
+++ second.c
@@ -256,9 +256,9 @@ int main(){
 
 //nested loop loaded>>>>>>>>>>
 
-for(int i=1;i<=5;i++){
-    printf("*");
-for(int j=1;j<=i;j++){
+for(int i=1;i<=4;i++){
+    printf("#");
+for(int j=1;j<=5;j++){
     printf("*");
 }
 printf("\n");
