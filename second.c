ac4f812fdbb895acf779c075372496166a51ea2a
sumit mahant <sumitmahant31@gmail.com>
2026-07-16 14:26:04 +0530

    ksdlsa

--- second.c
+++ second.c
@@ -255,13 +255,13 @@ int main(){
 //        }
 
 //nested loop loaded>>>>>>>>>>
-char ch='*';
-for(int i=1;i<=4;i++){
-    printf("%c\n",ch);
-for(char j=1;j<=7;j++){
-    printf("%c\n",ch);
-}
 
+for(int i=1;i<=5;i++){
+    printf("*");
+for(int j=1;j<=i;j++){
+    printf("*");
+}
+printf("\n");
 
 }
 }
