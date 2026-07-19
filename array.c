b391f511c125a5289f51958969dc43776e797f79
sumit mahant <sumitmahant31@gmail.com>
2026-07-19 18:48:27 +0530

    9090

--- array.c
+++ array.c
@@ -406,7 +406,12 @@ int main(){
 
 // printf(" address of the pointer =%u\n",&ptr);
 // printf(" value which store at address of age = %d",*(&age));
-
+int x;
+int *ptr;
+ptr = &x;
+*ptr=0;
+printf("value of x=%d\n",x);
+printf("value which store at the address=%d\n",*ptr);
 
 }
 
