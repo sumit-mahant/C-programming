40a3ec806b6677acf2f3ac6f3edfc2f2a865a018
sumit mahant <sumitmahant31@gmail.com>
2026-07-20 20:22:03 +0530

    last

--- array.c
+++ array.c
@@ -488,14 +488,29 @@
 // }
 int counteven(int arr[],int n);
 int reverce(int arr[],int n);
-int printarr(int arr[],int n);
+void printarr(int arr[],int n);
 int main(){
  int arr[]={1,2,3,4,5,6,7,8,9,10,12,14,16};
- printf("%d",counteven(arr,13));
-
+ printf("%d\n",counteven(arr,13));
+  reverce(arr,13);
+  printarr(arr,13);
 
 }
 
+void printarr(int arr[],int n){
+ for(int i=0;i<n;i++){
+    printf("%d\t",arr[i]);
+ }
+printf("\n");
+}
+int reverce(int arr[],int n){
+    for(int i=0;i< n/2;i++){
+        int firstvall=arr[i];
+        int secondvall=arr[n-i-1];
+        arr[i]=secondvall;
+        arr[n-i-1]=firstvall;
+    }
+}
 
 
 
@@ -511,6 +526,7 @@ for(int i=0;i<n;i++){
     return count;
 }
 
+//fibonacci series😁😁😁😁😁😁
 
 
 
