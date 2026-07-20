71ecb4e10caac0a5c18b66f0ae5b6bf18d13188e
sumit mahant <sumitmahant31@gmail.com>
2026-07-20 20:00:03 +0530

    new

--- array.c
+++ array.c
@@ -486,9 +486,29 @@
 //      }
 //          return count;
 // }
-void counteven(int arr[],int n){
+int counteven(int arr[],int n);
+int reverce(int arr[],int n);
+int printarr(int arr[],int n);
+int main(){
+ int arr[]={1,2,3,4,5,6,7,8,9,10,12,14,16};
+ printf("%d",counteven(arr,13));
 
-    
+
+}
+
+
+
+
+
+
+int counteven(int arr[],int n){
+int count=0;
+for(int i=0;i<n;i++){
+    if( arr[i]%2==0){
+        count++;
+    }
+}
+    return count;
 }
 
 
