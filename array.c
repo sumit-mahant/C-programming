cbea4501a96175c78323876b75bf274817cfe93d
sumit mahant <sumitmahant31@gmail.com>
2026-07-20 18:13:00 +0530

    funny

--- array.c
+++ array.c
@@ -444,13 +444,16 @@ void printnum(int arr[],int n);
 
 
 int main(){
-
+  int arr[]={20,30,40,50,60};
+  int n=5;
+  printnum(arr,5);
     return 0;
 }
 void printnum(int arr[],int n){
   for(int i=0;i<n;i++){
 printf("%d \t",arr[i]);
   }
+  printf("\n");
 }
 
 
