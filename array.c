b72f46e8b443853ef23842a1792eb271bcc7d093
sumit mahant <sumitmahant31@gmail.com>
2026-07-21 12:03:47 +0530

    21 july

--- array.c
+++ array.c
@@ -528,18 +528,18 @@
 
 //fibonacci series😁😁😁😁😁😁
 int main(){
-int n;
-printf("Enter a number(   <2=:)");
-scanf("%d",&n);
+// int n;
+// printf("Enter a number(   <2=:)");
+// scanf("%d",&n);
 
-int fib[n];
- fib[0]=0;
- fib[1]=1;
-for(int i=2;i<n;i++){
-    fib[i]=fib[i-1]+fib[i-2];
-    printf("%d\t",fib[i]);
-}
-printf("\n");
+// int fib[n];
+//  fib[0]=0;
+//  fib[1]=1;
+// for(int i=2;i<n;i++){
+//     fib[i]=fib[i-1]+fib[i-2];
+//     printf("%d\t",fib[i]);
+// }
+// printf("\n");
 
 
 }
